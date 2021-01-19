f753573b1cac153f2bff7b145224c38e71fc6a58
KaO <52933590+painteau@users.noreply.github.com>
2021-01-19 10:38:47 +0100

    Add files via upload

--- AutoOutro.bat
+++ AutoOutro.bat
@@ -1,9 +1,4 @@
 @echo off > NUL
-:: clean des files
-del /F /Q %mypath%\files\intermediaire.mp4
-del /F /Q %mypath%\files\clip.mp4
-cls
-
 
 :: set de la date
 set mypath=%cd%
@@ -17,23 +12,24 @@ set sec=8
 set /p id=Enter Clip URL (Ctrl + V then press Enter): 
 set /p sec=Enter duration of desired outro in seconds (Enter 4, 6 or 8, then press Enter): 
 
+for %%g in (%id%) do set "name=%%~nxg"
 
 :: Download file from twitch ....
 TITLE Download file from twitch ....
-%mypath%\files\youtube-dl --output %mypath%\files\clip.mp4 %id%
+%mypath%\files\youtube-dl --output %mypath%\files\%name%.mp4 %id%
 
 :: Encoding clip into 1080p60 ....
 TITLE Encoding clip into 1080p60 ....
-%mypath%\files\ffmpeg -i %mypath%\files\clip.mp4 -acodec libvo_aacenc -ar 48000 -ab 160k -ac 2 -vcodec libx264 -r 60 -s 1920x1080 -b:v 20000k -maxrate 20000k -bufsize 20000k -y %mypath%\files\intermediaire.mp4
+%mypath%\files\ffmpeg -i %mypath%\files\%name%.mp4 -acodec libvo_aacenc -ar 48000 -ab 160k -ac 2 -vcodec libx264 -r 60 -s 1920x1080 -b:v 20000k -maxrate 20000k -bufsize 20000k -y %mypath%\files\%name%_encoded.mp4
 
 :: Joining files together ....
 TITLE Joining files together ....
-%mypath%\files\ffmpeg -i %mypath%\files\intermediaire.mp4 -i %mypath%\files\outro%sec%s.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" %mypath%\FINAL-%madate%.mp4
+%mypath%\files\ffmpeg -i %mypath%\files\%name%_encoded.mp4 -i %mypath%\files\outro%sec%s.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" %mypath%\%name%-%madate%.mp4
 
 :: Deleting temporary files ....
 TITLE Deleting temporary files ....
-del /F /Q %mypath%\files\intermediaire.mp4
-del /F /Q %mypath%\files\clip.mp4
+del /F /Q %mypath%\files\%name%_encoded.mp4
+del /F /Q %mypath%\files\%name%.mp4
 cls
 TITLE Job is done !
 echo Job is done !
